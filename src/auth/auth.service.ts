2d56eaa4923a4c0c04731f296753ab3e28e07e88
Cristhian Forero <cristhianfdx@gmail.com>
2020-11-05 00:40:45 -0500

    Ajuste

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,4 @@
-import {
-  Injectable,
-  InternalServerErrorException,
-  UnauthorizedException,
-} from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
